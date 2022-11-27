cf72e9ac44c14a87d396c493d451eeed40c2e19e
Babukh Yurii <99011291+MercuryCHNU@users.noreply.github.com>
2022-11-28 01:33:51 +0200

    Add files via upload

---
+++ Lab5/Lab5_C_Sharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat c1 = new Cat("Marik", Gender.Male);
+
+
+            //test
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.CheckEnergy();
+            c1.Jump();
+            c1.Jump();
+            c1.Jump();
+            c1.CheckEnergy();
+            c1.Sleep();
+            c1.Sleep();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
