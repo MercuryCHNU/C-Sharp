2c9ce1d25afe2995c0ad87ac6ba6a37b1674e777
Babukh Yurii <99011291+MercuryCHNU@users.noreply.github.com>
2022-11-28 01:30:04 +0200

    Add files via upload

---
+++ Lab3/Lab3_C_Sharp/Car_class.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Car
+{
+    public const string CompanyName = "Havi-Motors";
+    private string name;
+    private string model;
+    private string color;
+    private double price;
+
+    //Властивість доступу до поля колір
+    public string Color
+    {
+        get { return color; }
+        set { color = value; }
+    }
+
+    //За умовчуванням
+    public Car():this("Name", "Model", "White", 10000)
+    {
+    }
+
+    //Конструктор з параметрами
+    public Car(string name, string model, string color, int price)
+    {
+        this.name = name;
+        this.model = model;
+        this.Color = color;
+        this.price = price;
+    }
+
+    //Ввід даних про авто
+    public void Input()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine("Введіть марку машини:");
+        this.name = Console.ReadLine();
+        Console.WriteLine("Введіть модель машини:");
+        this.model = Console.ReadLine();
+        Console.WriteLine("Введіть колір машини:");
+        this.Color = Console.ReadLine();
+        Console.WriteLine("Введіть ціну машини:");
+        this.price = Convert.ToDouble(Console.ReadLine());
+    }
+
+    //Вивід даних про авто
+    public void Print()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine($"Company name: {Car.CompanyName} Name: {this.name} " +
+            $"Model: {this.model} Color: {this.Color} Price: {this.price}");
+    }
+
+    //Перевірка чи збігається марка та колір вказаних автомобілів
+    public static bool operator ==(Car car1, Car car2)
+    {
+        return (car1.name == car2.name) && (car1.price == car2.price);
+    }
+    public static bool operator !=(Car car1, Car car2)
+    {
+        return (car1.name != car2.name) && (car1.price != car2.price);
+    }
+
+    //Вивід данних про авто за допомогою ToString
+    public override string ToString()
+    {
+        return $"Company name: {Car.CompanyName} Name: {this.name} " +
+            $"Model: {this.model} Color: {this.Color} Price: {this.price}";
+    }
+
+    //додає x% до ціни автомобіля
+    public void ChangePrice(double x)
+    {
+        price += price * x / 100;
+    }
+}
+
---
+++ Lab3/Lab3_C_Sharp/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Натисніть любу кнопку для того щоб побачити дані про Car");
+            Console.ReadKey();
+
+            //Cars
+            Car car1 = new Car("Audi", "A6", "Black", 14600);
+            Car car2 = new Car("Voltswagen", "Passat B5", "Silver", 12500);
+            Car car3 = new Car("BMW", "528", "Blue", 13400);
+
+            car1.Print();
+            car1.ChangePrice(10);
+            car1.Print();
+
+            car2.Print();
+            car2.ChangePrice(10);
+            car2.Print();
+
+            car3.Print();
+            car3.ChangePrice(10);
+            car3.Print();
+
+            Console.WriteLine(car1 == car2);
+            Console.WriteLine(car2 == car3);
+
+            Console.WriteLine("Натисніть любу кнопку для того щоб побачити дані про Person");
+            Console.ReadKey();
+
+            //Person
+            Person p1 = new Person("Andrii", 2004);
+            Person p2 = new Person("Denis", 1995);
+            Person p3 = new Person("Elizabeth", 1997);
+            Person p4 = new Person("Andrii", 2009);
+            Person p5 = new Person("Alina", 2005);
+            Person p6 = new Person("John", 2000);
+
+            p1.Age();
+            p2.Age();
+            p3.Age();
+            p4.Age();
+            p5.Age();
+            p6.Age();
+
+            p1.Output();
+            p2.Output();
+            p3.Output();
+            p4.Output();
+            p5.Output();
+            p6.Output();
+
+            p4.ChangeName("Very Young");
+            p4.Output();
+
+            Console.WriteLine($"Інформація про всіх осіб: {p1}; {p2}; {p3}; {p4}; {p5}; {p6}");
+
+            p1.Comparison(p1, p4);
+            p1.Comparison(p1, p6);
+
+            Console.WriteLine("Натисніть любу кнопку для того щоб закрити консоль");
+            Console.ReadKey();
+        }
+    }
+}
